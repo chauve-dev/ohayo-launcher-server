aeee40c872f71c00595216b394d539089835cfba
Bricklou <louis.bailleau@free.fr>
2020-04-01 14:07:21 +0200

    Mise à jours de la configuration
    - Mise à jours de tsconfig.json
    - Ajout de l'auto fix de ESLint dans la nuxt.config.js
    - Ajout de la configuration de typescript dans nuxt.config.js
    - Mise à jours du package.json pour typescript et serverMiddleware
    - Création d'un fichier pour le serverMiddleware

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,32 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(ts|js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  /*
+   ** Typescript module configuration
+   ** See https://typescript.nuxtjs.org
+   */
+  typescript: {
+    typeCheck: {
+      eslint: true
+    }
+  },
+
+  /*
+   ** Server middleware configuration
+   ** See https://nuxtjs.org/api/configuration-servermiddleware
+   */
+  serverMiddlware: ['~/api/index.ts']
 }
