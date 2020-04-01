3e8928600550bc354907fc89b03b7cfe93673382
Bricklou <louis.bailleau@free.fr>
2020-04-01 16:11:02 +0200

    Créé la base de l'application Express
    - Mise en place du serveur Express dans `api/index.ts`

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,14 @@
-//
+import express, { Application } from 'express'
+
+/**
+ * Configuring Express application
+ */
+const app: Application = express()
+
+/**
+ * Export application for the NuxtJS serverMiddleware option
+ */
+export default {
+  path: '/api',
+  handler: app,
+}
